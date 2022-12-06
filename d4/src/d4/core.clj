2e6b4447dcf54fde0a3edb328494ad5054b3eb65
Nico Roos <nicoroos@Nicos-MBP.localdomain>
2022-12-06 20:24:21 +0200

    day 4

--- d4/src/d4/core.clj
+++ d4/src/d4/core.clj
@@ -1,15 +1,39 @@
 (ns d4.core)
 
-(def test-input
-  ["2-4,6-8"
-  "2-3,4-5"
-  "5-7,7-9"
-  "2-8,3-7"
-  "6-6,4-6"
-  "2-6,4-8"])
+(defn parse-ranges
+  [line]
+  (map (fn [x] (Integer/parseInt x)) (clojure.string/split line #"-")))
 
-(let [[a b] (clojure.string/split (first test-input) #",")]
-  a)
+(defn solve
+  [lines amount]
+  (if (empty? lines)
+    amount
+    (let [[a b] (clojure.string/split (first lines) #",")
+          [a-start a-end] (parse-ranges a)
+          [b-start b-end] (parse-ranges b)]
+      (recur
+        (rest lines)
+        (if (or (<= a-start b-start b-end a-end)
+                (<= b-start a-start a-end b-end))
+          (+ amount 1)
+          amount)))))
+
+(defn solve-2
+  [lines amount]
+   (if (empty? lines)
+     amount
+     (let [[a b] (clojure.string/split (first lines) #",")
+           [a-start a-end] (parse-ranges a)
+           [b-start b-end] (parse-ranges b)]
+       (recur
+         (rest lines)
+         (if (or (<= a-start b-start a-end)
+                 (<= b-start a-start b-end))
+           (+ amount 1)
+           amount)))))
+
+(solve (clojure.string/split-lines (slurp "/tmp/input.txt")) 0)
+(solve-2 (clojure.string/split-lines (slurp "/tmp/input.txt")) 0)
 
 (defn foo
   "I don't do a whole lot."
